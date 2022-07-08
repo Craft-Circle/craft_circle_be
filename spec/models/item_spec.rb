b413480678c9fd15e565cb07214d9cab1df0bd3a
Sherman Au <shermau13@gmail.com>
2022-07-08 16:17:18 -0600

    test: item validations and relationships

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:available) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:type) }
+    it { should validate_numericality_of(:amount) }
+    it { should validate_inclusion_of(:available).in?([true, false]) }
+  end
 end

e37fecd669a9d7967d075e3d09a3a2bfd901822f
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-09 17:13:30 -0600

    Refactor: change item validation from type to category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :name, :description, :available, :type, :status
+  validates_presence_of :name, :description, :available, :category, :status
   validates_numericality_of :amount
   validates :available, inclusion: { in: [true, false] }
 

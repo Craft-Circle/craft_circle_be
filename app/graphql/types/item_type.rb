c492dfa02fa92d8aee39b73f331524ce335fdd88
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-09 19:22:58 -0600

    Create Item Type for GraphQL

---
+++ app/graphql/types/item_type.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Types
+  class ItemType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :category, String, null: false
+    field :description, String, null: false
+    field :available, Boolean, null: false
+    field :amount, Integer, null: false
+    field :status, String, null: false
+    field :user_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end

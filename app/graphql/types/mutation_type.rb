3d37a4499cfbc75a889f28c2e84ea62350e8ef9e
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-09 19:23:39 -0600

    Add GraphQL Querytypes for users and items

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    # # TODO: remove me
+    # field :user_field, Object, null: false,
+    #   description: "All users"
+    # def user_field
+    #   User.all
+    # end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,45 @@
+# module Types
+#   class QueryType < Types::BaseObject
+#     # Add root-level fields here.
+#     # They will be entry points for queries on your schema.
+#
+#     field :fetch_notes, resolver: Queries::FetchNotes
+#   end
+# end
+
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :get_users, [Types::UserType], null: false, description: "Returns a list of users"
+    def get_users
+      User.all
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :get_a_user, Types::UserType, null: false, description: "Returns a user" do
+      argument :id, ID, required: true
+    end
+    def get_a_user(id:)
+      User.find(id)
+    end
+
+    field :get_items, [Types::ItemType], null: false, description: "Returns all items"
+    def get_items
+      Item.all
+    end
+
+    field :get_user_items, [Types::ItemType], null: false, description: "Returns a users items" do
+      argument :id, ID, required: true
+    end
+    def get_user_items(id:)
+      @user = User.find(id)
+      @user.items
+    end
+
+    field :item_search, [Types::ItemType], null: false, description: "Returns an array of items with a similar name" do
+      argument :keyword, String, required: true
+    end
+    def item_search(keyword:)
+      items = Item.where("name ILIKE ?", "%#{keyword}%")
     end
   end
 end

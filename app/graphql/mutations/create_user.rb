285ee8b5c04cfc2a87d8823ace9e9f85dae48464
Joseph Downs <jos.w.downs@gmail.com>
2022-07-13 15:58:16 -0600

    Add mutation to create user
    Created create_user.rb

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,14 @@
+module Mutations 
+  class CreateUser < BaseMutation
+    argument :name, String, required: true
+    argument :email, String, required: true
+    field :user, Types::UserType, null: false
+
+    def resolve(**args) 
+      new_user = User.create!(args)
+      # if new_user.save
+        {user: new_user}
+      # end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_user, mutation: Mutations::CreateUser
     # # TODO: remove me
     # field :user_field, Object, null: false,
     #   description: "All users"

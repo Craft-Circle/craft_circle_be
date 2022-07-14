ba39a5201502991ccaa6ae77ea636dd2da3366b1
Phillip Kamps <phillip.kamps@gmail.com>
2022-07-13 18:09:10 -0600

    Adds mutation to create item Wrote a mutation that receives attributes and creates item that belongs to user

---
+++ app/graphql/mutations/create_item.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateItem < BaseMutation
+    argument :name, String, required: false
+    argument :category, Integer, required: false
+    argument :description, String, required: false
+    argument :available, Boolean, required: false
+    argument :amount, Integer, required: false
+    argument :status, Integer, required: false
+    argument :user_id, ID, required: false
+    field :item, Types::ItemType, null: false
+
+    def resolve(**args)
+      new_item = Item.create!(args)
+      { item: new_item }
+    end
+  end
+end

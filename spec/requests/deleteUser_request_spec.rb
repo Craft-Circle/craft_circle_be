3c048decfdd497600c1462f30300fd84608fa7e7
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-13 18:28:02 -0600

    Test: deleteUser mutation Test the deleteUser mutation to ensure it always works

---
+++ spec/requests/deleteUser_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'deleteUser', type: :request do
+  it "creates a user" do
+    test_user = User.create!(
+      id: 1,
+      name: "User 1",
+      email: "one@email.com"
+    )
+
+    expect(test_user).to be_a(User)
+    expect(test_user.name).to eq("User 1")
+    expect(User.all).to include(test_user)
+
+    deleter_mutation =
+      <<~GQL
+        mutation deleteUser {
+            deleteUser(input:{id: #{test_user.id}}) {
+              success
+            }
+          }
+      GQL
+    post '/graphql', params: { query: deleter_mutation }
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:deleteUser][:success]).to be true
+    expect(User.all.empty?).to be true
+  end
+end

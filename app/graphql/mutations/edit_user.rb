eaaa4c0be8a49ccea8aee9e4c20542a9b875f07a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-14 19:10:26 -0600

    editUser mutation invalid ID Rescue editUser mutation when the user cannot be found or doesnt exist

--- app/graphql/mutations/edit_user.rb
+++ app/graphql/mutations/edit_user.rb
@@ -3,14 +3,19 @@ module Mutations
     argument :id, ID, required: true
     argument :name, String, required: false
     argument :email, String, required: false
-    field :user, Types::UserType, null: false
+    field :user, Types::UserType
+    field :error, String
 
     def resolve(id:, **args)
       updated_user = User.find(id)
+
       updated_user.update(args)
       if updated_user.save
         { user: updated_user }
       end
+
+    rescue ActiveRecord::RecordNotFound => error
+      GraphQL::ExecutionError.new("User with ID #{id} could not be found or doesn't exist")
     end
   end
 end

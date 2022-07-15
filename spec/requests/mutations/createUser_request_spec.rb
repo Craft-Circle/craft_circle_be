1ad24841a88f2cacb9f9ef392f99c86e6def4fe8
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-14 20:41:39 -0600

    Test: userCreate mutation email taken Spec test to ensure when a user tries to create an account with a taken email, a familiar error is raised

--- spec/requests/mutations/createUser_request_spec.rb
+++ spec/requests/mutations/createUser_request_spec.rb
@@ -20,4 +20,27 @@ RSpec.describe 'createUser', type: :request do
       expect(data[:email]).to eq("one@email.com")
       expect(User.last.name).to eq("User one")
   end
+
+  it "fails gracefully when a a user with the entered email already exists" do
+    user_1 = User.create!(
+      name: "new user",
+      email: "one@email.com"
+    )
+    error_mutation =
+      <<~GQL
+        mutation createUser {
+            createUser(input:{name: "User one", email: "one@email.com"}) {
+              user {
+                name
+                email
+              }
+            }
+          }
+      GQL
+      post '/graphql', params: { query: error_mutation }
+      json = JSON.parse(response.body, symbolize_names: true)
+      error = json[:errors][0]
+
+      expect(error[:message]).to eq("#{user_1.email} is already associated with another account. Your email must be unique.")
+  end
 end

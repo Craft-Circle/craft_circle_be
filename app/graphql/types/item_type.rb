75e419d08bf0084784827d2cb8b587e6565abf8f
Sherman Au <shermau13@gmail.com>
2022-07-14 23:32:35 -0600

    Refactor: change user_id: field to user:
    Allows access to all attributes from a User instead of just the ID for more dynamic use of data

--- app/graphql/types/item_type.rb
+++ app/graphql/types/item_type.rb
@@ -9,7 +9,7 @@ module Types
     field :available, Boolean, null: false
     field :amount, Integer, null: false
     field :status, String, null: false
-    field :user_id, ID, null: false
+    field :user, Types::UserType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end

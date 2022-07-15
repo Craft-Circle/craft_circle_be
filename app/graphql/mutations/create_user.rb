21965b69bacaf674128e2ef00e4c0e1cb3a099bb
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-14 21:00:23 -0600

    userCreate mutation blank name or email ensure when the userCreate mutation is fed empty strings, it returns a familiar error

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,14 +1,18 @@
-module Mutations 
+module Mutations
   class CreateUser < BaseMutation
     argument :name, String, required: true
     argument :email, String, required: true
     field :user, Types::UserType, null: false
 
-    def resolve(**args) 
+    def resolve(**args)
+      if args[:name].empty? || args[:email].empty?
+        raise GraphQL::ExecutionError.new("Name and email must not be left blank.")
+      end
       new_user = User.create!(args)
-      # if new_user.save
-        {user: new_user}
-      # end
+      {user: new_user}
+
+    rescue ActiveRecord::RecordInvalid => error
+      GraphQL::ExecutionError.new("#{args[:email]} is already associated with another account. Your email must be unique.")
     end
   end
 

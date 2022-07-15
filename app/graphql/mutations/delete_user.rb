b442564202f3aa526c341c59dd193528d3ad4b18
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-14 20:26:42 -0600

    deleteUser user not found When the deleteUser mutation cannot find the user, it returns a familiar error

--- app/graphql/mutations/delete_user.rb
+++ app/graphql/mutations/delete_user.rb
@@ -2,7 +2,7 @@ module Mutations
   class DeleteUser < BaseMutation
     argument :id, ID, required: true
     field :success, Boolean
-    
+
     def resolve(id:)
       user = User.find(id)
       if user.destroy
@@ -10,6 +10,9 @@ module Mutations
       else
         {success: false}
       end
+
+    rescue ActiveRecord::RecordNotFound => error
+      GraphQL::ExecutionError.new("User with ID #{id} could not be found or doesn't exist")
     end
   end
 end

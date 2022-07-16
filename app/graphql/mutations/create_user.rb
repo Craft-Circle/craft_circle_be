e1c66b1b28281910a8404a6c2c13841e49a6489b
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-16 16:28:29 -0600

    Refactor: remove createUser mutation edge case handling Back End decided the let GraphQL handle the edge cases as it has buily-in error handling which results in a response very similar, but already more precise, than our custom error handling

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -5,14 +5,8 @@ module Mutations
     field :user, Types::UserType, null: false
 
     def resolve(**args)
-      if args[:name].empty? || args[:email].empty?
-        raise GraphQL::ExecutionError.new("Name and email must not be left blank.")
-      end
       new_user = User.create!(args)
       {user: new_user}
-
-    rescue ActiveRecord::RecordInvalid => error
-      GraphQL::ExecutionError.new("#{args[:email]} is already associated with another account. Your email must be unique.")
     end
   end
 
--- spec/requests/mutations/createUser_request_spec.rb
+++ spec/requests/mutations/createUser_request_spec.rb
@@ -20,46 +20,4 @@ RSpec.describe 'createUser', type: :request do
       expect(data[:email]).to eq("one@email.com")
       expect(User.last.name).to eq("User one")
   end
-
-  it "fails gracefully when a a user with the entered email already exists" do
-    user_1 = User.create!(
-      name: "new user",
-      email: "one@email.com"
-    )
-    error_mutation =
-    <<~GQL
-      mutation createUser {
-          createUser(input:{name: "User one", email: "one@email.com"}) {
-            user {
-              name
-              email
-            }
-          }
-        }
-    GQL
-    post '/graphql', params: { query: error_mutation }
-    json = JSON.parse(response.body, symbolize_names: true)
-    error = json[:errors][0]
-
-    expect(error[:message]).to eq("#{user_1.email} is already associated with another account. Your email must be unique.")
-  end
-
-  it "fails gracefully when a user does not enter an email or name" do
-    error_mutation =
-    <<~GQL
-      mutation createUser {
-          createUser(input:{name: "", email: ""}) {
-            user {
-              name
-              email
-            }
-          }
-        }
-    GQL
-    post '/graphql', params: { query: error_mutation }
-    json = JSON.parse(response.body, symbolize_names: true)
-    error = json[:errors][0]
-
-    expect(error[:message]).to eq("Name and email must not be left blank.")
-  end
 end

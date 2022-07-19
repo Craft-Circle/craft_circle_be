a3570d3251154ec7acc2932ad10f97362388f4cb
Sherman Au <shermau13@gmail.com>
2022-07-18 18:28:10 -0600

    Fix: Heroku Gods please

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'factory_bot_rails'
-
 Item.destroy_all
 User.destroy_all
 
@@ -8,5 +6,3 @@ users = FactoryBot.create_list(:user, 10)
 users.each do |user|
   FactoryBot.create_list(:item, 5, { user_id: user.id })
 end
-
-
